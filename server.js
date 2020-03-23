65443a66b62b44a8e1e03fefe64de7580c50baf3
Yurii <yurageyvakh@gmail.com>
2020-03-23 13:19:00 +0200

    Added morgan, apply cors and body-parser package

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+//aply middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors({origin: process.env.CLIENT_URL}));
 
 app.use('/api', authRoutes);
 
