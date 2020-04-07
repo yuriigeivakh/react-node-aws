c4aab5dbe78c73b1225dc1ef083be601631c8bf7
Yurii <yurageyvakh@gmail.com>
2020-04-07 11:22:28 +0300

    Category schema

---
+++ models/category.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 32,
+        },
+        slug: {
+            type: String,
+            lowercase: true,
+            required: true,
+            unique: true,
+            index: true,
+        },
+        image: {
+            url: String,
+            key: String,
+        },
+        content: {
+            type: {},
+            min: 20,
+            min: 2000,
+        },
+        postedBy: {
+            type: ObjectId,
+            ref: 'User',
+            min: 2000,
+        },
+    },
+    { timestamps: true },
+);
+
+
+module.exports = mongoose.model('Category', categorySchema);
