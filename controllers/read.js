30bd083726c318ff6a0a3a18b2d359f14328ecf1
Yurii <yurageyvakh@gmail.com>
2020-05-08 13:38:14 +0300

    All links by user

--- controllers/read.js
+++ controllers/read.js
@@ -1,6 +1,19 @@
+const User = require('../models/user');
+const Link = require('../models/link');
 
 exports.read = (req, res) => {
-    req.profile.hashed_password = undefined;
-    req.profile.salt = undefined;
-    res.json(req.profile)
+    User.findOne({_id: req.user._id}).exec((err, user) => {
+        if (err) return res.status(404).json({error: 'Could not find user'});
+        Link
+            .find({postedBy: user})
+            .populate('categories', 'name slug')
+            .populate('postedBy', 'name')
+            .sort({createdAt: -1})
+            .exec((err, links) => {
+                if (err) res.status(400).json({ error: 'Could not load links posted by user' });
+                user.hashed_password = undefined;
+                user.salt = undefined;
+                res.json({user, links});
+            })
+    })
 }
